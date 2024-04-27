561523d4be2c6343ac0f4aed869ff9650f2ad3e4
Maksym Tkachuk <tkachykmakc@gmail.com>
2024-04-27 19:18:37 +0300

    Updates

--- git-scripts/helpers/cleanConsoleLine.ts
+++
@@ -1,3 +0,0 @@
-export const cleanConsoleLine = (): void => {
-  process.stdout.write(`\r${' '.repeat(process.stdout.columns - 1)}\r`)
-}
--- git-scripts/helpers/syncRunTrimmed.ts
+++
@@ -1,5 +0,0 @@
-import { execSync } from 'child_process'
-
-export const syncRunTrimmed = (command: string): string => {
-  return execSync(command).toString().trim()
-}
